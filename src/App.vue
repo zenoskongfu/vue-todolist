cc27540c643a39a75312c10153a5e8caa89212af
zenos <zenos.chen@foxmail.com>
2024-12-07 19:25:10 +0800

    update code

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Hi, 王诗迪同学, 这里是TODOLIST的起始页</h1>
-  </div>
+	<div>
+		<h1>Hi, 王诗迪同学, 这里是TODOLIST的起始页</h1>
+	</div>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+	filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+	filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
