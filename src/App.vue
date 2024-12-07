83ab1cbc2af913ba592384f4838032e8cd53b369
zenos <zenos.chen@foxmail.com>
2024-12-07 13:18:08 +0800

    repo init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Hi, 王诗迪同学, 这里是TODOLIST的起始页</h1>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
