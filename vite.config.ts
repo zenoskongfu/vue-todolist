8d4037740afcd8152bb58618f0595737d343a187
程同学 <zenos.chen@foxmail.com>
2024-12-17 23:07:11 +0800

    update code

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "todolist",
+	base: "/todolist",
 	plugins: [vue()],
 	server: {
 		port: 80,
